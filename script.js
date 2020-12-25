b3a9a1bcda2327e8ced8ff8dd7d676b07e3aeba6
0anirban1 <64486122+0anirban1@users.noreply.github.com>
2020-12-25 17:13:57 +0530

    updated once again

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 function openNav() {
+
+  if(window.matchMedia("(max-width : 450px)").matches){
+    document.getElementById("mySidenav").style.width = "411px";
+  }else{
     document.getElementById("mySidenav").style.width = "280px";
     document.getElementById("mySidenav").style.display = "block";
     document.getElementById("main").style.paddingLeft = "280px";
     document.getElementById("initNav").style.paddingLeft = "300px";
     document.getElementById("openMenu").style.display = "none";
   }
+}
   
   /* Set the width of the side navigation to 0 */
   function closeNav() {
@@ -13,5 +18,6 @@ function openNav() {
     document.getElementById("initNav").style.paddingLeft = "0px";
     document.getElementById("openMenu").style.display = "inline";
   }
+
 
   
